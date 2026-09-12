071f1ffda8b5b15aae59949555d1ec2930a42b5b
Stefan Bergli <ascendens@hotmail.com>
2026-09-12 08:31:44 +0200

    Färdig

--- src/main/java/Caranddrivingdistans.java
+++ src/main/java/Caranddrivingdistans.java
@@ -7,17 +7,16 @@ public class Caranddrivingdistans {
         Bil bil1 = new Bil("Volvo", "740", 15000 );
         Bil bil2 = new Bil("Suzuki", "Swift", 900);
 
-        IO.println(kontrollera(bil1, gräns));
+       IO.println( kontrollera(bil1, gräns));
         IO.println(kontrollera(bil2, gräns));
-
-
 
     }
     static String kontrollera(Bil b, int gräns){
-        if (gräns > b.mil());
+
+     if ( gräns > b.mil())
         return "Bilen har gått under gränsen";
 
-    return "Bilen har gått över gränsen";
+     return "Bilen har gått över gränsen";
 
     }
 
